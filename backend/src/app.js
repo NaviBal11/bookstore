5fb4ada4e706022a165d1fd1a9fac565a5d45557
Navdeep Bal <navikainth11@gmail.com>
2024-09-27 10:25:18 -0600

    lots of changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,14 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = ["https://bookstorefrontend-eta.vercel.app"];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
