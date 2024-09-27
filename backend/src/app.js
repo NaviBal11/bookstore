e8bd62d28aceb813611497379834bc2a7bf90dca
Navdeep Bal <navikainth11@gmail.com>
2024-09-27 10:35:36 -0600

    remove frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,14 +4,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-const allowedOrigins = ["https://bookstorefrontend-eta.vercel.app"];
-
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
