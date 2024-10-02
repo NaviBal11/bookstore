4b18c6161e86df2d764c64f1b40b6b7373c8ea44
Navdeep Bal <navikainth11@gmail.com>
2024-10-02 17:38:35 -0600

    make more changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
   plugins: [react()],
+  base: "/",
   build: {
     outDir: "dist",
-    assetsDir: "assets",
   },
-  base: "/",
 });
