e9f61919867041bb7756a13c12060dbb7c667e8b
Navdeep Bal <navikainth11@gmail.com>
2024-10-02 08:38:20 -0600

    add frontend url to success

--- backend/src/controllers/stripe.controller.js
+++ backend/src/controllers/stripe.controller.js
@@ -32,8 +32,8 @@ const payment = async (req, res) => {
         tax_rates: [taxRate.id],
       })),
       mode: "payment",
-      success_url: "https://bookstore-frontend-57tw.onrender.com/success", // Replace with your success URL
-      cancel_url: "https://bookstore-frontend-57tw.onrender.com/cart", // Replace with your cancel URL
+      success_url: "https://spiffy-bonbon-cfabc9.netlify.app/success", // Replace with your success URL
+      cancel_url: "https://spiffy-bonbon-cfabc9.netlify.app/cart", // Replace with your cancel URL
     });
 
     // Calculate total amount
